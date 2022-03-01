65874c1c70984f4d05588bf993851e1dba73f0cd
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-03-01 14:57:27 +0530

    Time: 8 ms (52.50%), Space: 7.9 MB (48.88%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,23 +1,15 @@
 class Solution {
-public:
-    int ones(int n){
-        int cnt=0;
-        
-        while(n){
-            if(n&1)
-                cnt++;
-            n = n>>1;
-        }
-        
-        return cnt;
-    }
+
 public:
     vector<int> countBits(int n) {
         
         vector<int> ans(n+1,0);
         
         for(int i=0; i<n+1; i++){
-            ans[i] = ones(i);
+            if(i%2==0)
+                ans[i] = ans[i/2];
+            else
+                ans[i] = ans[i/2]+1;
         }
         
         return ans;
