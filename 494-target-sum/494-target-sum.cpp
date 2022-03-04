1911e62e0b3066d9f295534e40fbd72ce112aca2
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-03-04 18:20:25 +0530

    Time: 16 ms (69.74%), Space: 12.4 MB (42.25%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int f(int n, int sum, vector<int> &a, vector<vector<int>> &dp){
+        if(n==0)
+            return sum==0;
+        
+        if(dp[n][sum] != -1)
+            return dp[n][sum];
+        
+        if(a[n-1] <= sum)
+            return dp[n][sum] = f(n-1,sum-a[n-1],a,dp) + f(n-1,sum,a,dp);
+        return dp[n][sum] = f(n-1, sum,a,dp);
+    }
+    int findTargetSumWays(vector<int>& nums, int diff) {
+        
+        int n = nums.size(), total = 0;
+        for(auto i: nums)
+            total += i;
+        //s1 - s2 = diff
+        //s1 - (total - s1) = diff
+        //2*s1 = diff + total 
+        //s1 = (diff + total)/2
+        if((diff + total)%2 != 0)
+            return 0;
+        
+        int sum = (diff + total)/2;
+        if(sum>total || sum<0) return 0;
+        //find how many ways we can have a subset with given sum
+        //count subset sum
+        vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
+        return f(n,sum,nums,dp);
+    }
+};
