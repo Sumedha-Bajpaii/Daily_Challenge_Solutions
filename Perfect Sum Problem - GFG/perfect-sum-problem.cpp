a9816c67a0697ff6de1564332e76ac847dbd51d8
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-03-04 12:15:42 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -3,31 +3,32 @@
 using namespace std;
 
  // } Driver Code Ends
+
+const unsigned int M = 1000000007;
 class Solution{
 	public:
 	int f(int n, int sum, int arr[], vector<vector<int>> &dp){
+	   // if(sum==0)
+	   //     return 1;
 	    if(n==0)
 	        return sum==0;
-	       
+	    
 	    if(dp[n][sum] != -1)
 	        return dp[n][sum];
-	       
-	    int take=0, nottake=0;
-	    if(arr[n-1]<=sum)
-	        take = f(n-1,sum-arr[n-1],arr,dp);
-	    nottake = f(n-1,sum, arr, dp);
 	    
-	    return dp[n][sum] = (take+nottake)%1000000007;
+	    if(arr[n-1]<=sum)
+	        return dp[n][sum] = ( f(n-1,sum-arr[n-1],arr,dp) + f(n-1,sum,arr,dp) )%M;
+	    return dp[n][sum] = f(n-1,sum,arr,dp)%M;
+	    
 	}
 	int perfectSum(int arr[], int n, int sum)
 	{
         // Your code goes here
         vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
-        return f(n, sum, arr, dp);
+        return f(n,sum,arr,dp);
 	}
 	  
 };
-
 // { Driver Code Starts.
 int main() 
 {
