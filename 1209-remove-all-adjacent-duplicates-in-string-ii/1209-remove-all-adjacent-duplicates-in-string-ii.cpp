dbdec5a7a6c75eee1517cac8ec2c2e79396ff5f0
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-03-07 21:26:28 +0530

    Time: 16 ms (93.92%), Space: 11 MB (29.03%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,31 +1,22 @@
+//Two pointers Solution
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
         
-        stack<pair<char,int>> st;
+        int n = s.size(), j=0;
+        vector<int> cnt(n);
         
-        for(auto &c: s){
-            if(st.empty() or st.top().first!=c){
-                st.push({c,1});
-            }
-            else{
-                st.top().second++;
-                if(st.top().second == k){
-                        st.pop();
-                }
-            }
+        for(int i=0; i<n; i++,j++){
+            s[j] = s[i];
+            
+            if(j>0 && s[j]==s[j-1])
+                cnt[j] = cnt[j-1] + 1;
+            else
+                cnt[j] = 1;
+            
+            if(cnt[j]==k)
+                j -= k;
         }
-        string res = "";
-        while(!st.empty()){
-            char x = st.top().first;
-            int freq = st.top().second;
-            while(freq){
-                res += x;
-                freq--;
-            }
-            st.pop();
-        }
-        reverse(res.begin(),res.end());
-        return res;
+        return s.substr(0,j);
     }
 };
