7d89b1c150a7a5377064031b41033f3921f5245e
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-03-22 23:09:02 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (64.03%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,48 +1,24 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
+       
+        unordered_map<char,int> mp;
+        
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            mp[s[i]] = i;
+        }
         
         vector<int> ans;
         
-        int n = s.size();
-        unordered_map<char,vector<int> > mp;
+        int prev=-1, maxi=0;
         
-        for(int i=0; i<n; i++)
-        {
-            if(mp.find(s[i]) == mp.end())
-                mp[s[i]] = {i,i};
-            else
-                mp[s[i]][1] = i;
-        }
-        
-        vector<vector<int>> intervals;
-        for(auto p: mp)
-            intervals.push_back(p.second);
-        
-        sort(intervals.begin(),intervals.end());
-        
-        
-        // for(auto v: intervals)
-        //     cout<<v[0]<<" "<<v[1]<<endl;
-        
-        
-        vector<int> cur = intervals[0];
-        vector<vector<int>> merged;
-        
-        for(int i=1; i<intervals.size(); i++)
-        {
-            auto v = intervals[i];
-            if(cur[1] > v[0])
-                cur[1] = max(cur[1],v[1]);
-            else{
-                merged.push_back(cur);
-                cur = v;
+        for(int cur=0; cur<n; cur++){
+            maxi = max(maxi, mp[s[cur]]);
+            if(cur == maxi){
+                ans.push_back(maxi - prev);
+                prev = maxi;
             }
-        }
-        merged.push_back(cur);
-        for(auto v: merged){
-            // cout<<v[0]<<" "<<v[1]<<endl;
-            ans.push_back(v[1]-v[0]+1);
         }
         
         return ans;
