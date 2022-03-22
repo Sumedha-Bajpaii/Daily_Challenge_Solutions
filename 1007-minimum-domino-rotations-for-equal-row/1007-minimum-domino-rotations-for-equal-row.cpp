046343a74153ab16180acf451d3553f9dc2f0316
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-03-22 23:57:39 +0530

    Time: 242 ms (19.74%), Space: 111.5 MB (95.19%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
-    int minDominoRotations(vector<int>& A, vector<int>& B) {
-  auto top = A[0], bot = B[0], top1 = 0, bot1 = 0, top2 = 0, bot2 = 0;
-  for (auto i = 0; i < A.size(); ++i) {
-    if (A[i] != top && B[i] != top) top = 0;
-    if (A[i] != bot && B[i] != bot) bot = 0;
-    top1 += A[i] == top;
-    bot1 += B[i] == top;
-    top2 += A[i] == bot;
-    bot2 += B[i] == bot;
-  }
-  return top || bot ? min(A.size() - max(top1, bot1), A.size() - max(top2, bot2)) : -1;
-}
+    int f(vector<int> &one,vector<int> &two,int targ){
+        
+        int n = one.size();
+        int cnt=0;
+        for(int i=0; i<n; i++){
+            if(one[i]!=targ)
+            {
+                if(two[i]==targ)
+                    cnt++;
+                else
+                    return INT_MAX;
+            }
+        }
+        return cnt;
+    }
+    
+    int minDominoRotations(vector<int>& top, vector<int>& bot) {
+        
+        int ans=INT_MAX;
+        
+        ans = min(ans, f(top,bot,top[0]) );
+        ans = min(ans, f(top,bot,bot[0]) );
+        ans = min(ans, f(bot,top,top[0]) );
+        ans = min(ans, f(bot,top,bot[0]) );
+        
+        if(ans==INT_MAX)
+            return -1;
+        else
+            return ans;
+    }
 };
-
