9d4e49617f0f37583c55e55d7dbbd427bc7f8d0f
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-03-25 23:29:41 +0530

    Time: 26 ms (5.79%), Space: 10 MB (12.03%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -6,17 +6,11 @@ public:
     
     int twoCitySchedCost(vector<vector<int>>& costs) {
     
-        int n = costs.size();
         sort(costs.begin(),costs.end(),cmp);
-        
-//         for(auto v: costs)
-//             cout<<v[0]-v[1]<<" ";
-        
-        int res=0;
+        int n = costs.size(), res=0;
         
         for(int i=0; i<n/2; i++){
-            res += costs[i][0];
-            res += costs[n-1-i][1];
+            res += costs[i][0] + costs[n-1-i][1];
         }
         return res;
     }
