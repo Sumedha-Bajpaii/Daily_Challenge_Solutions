570ade9743809b1b821523dfbf3596226d4b29ad
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-03-27 15:14:28 +0530

    Time: 1210 ms (45.01%), Space: 269.4 MB (7.40%) - LeetHub

--- 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    int f(int ind, int num, int len, string &s, vector<vector<int>> &dp){
+    int f(int ind,int num,int len,string &s,vector<int> &suffix,vector<vector<int>> &dp){
         
         if(ind>=s.size())
             return 0;
         
         if(num==0)
-        {
-            int ones = 0;
-            for(int k=ind; k<s.size(); k++)
-                if(s[k]=='1')
-                    ones++;
-            return ones;
-        }
+            return suffix[ind];
         
         if(dp[ind][num] != -1)
             return dp[ind][num];
         
-        int take = f(ind+len,num-1,len,s,dp);
-        int notTake = f(ind+1,num,len,s,dp);
+        int take = f(ind+len,num-1,len,s,suffix,dp);
+        int notTake = f(ind+1,num,len,s,suffix,dp);
         if(s[ind]=='1') notTake++;
         
         return dp[ind][num] = min(take,notTake);
@@ -26,8 +20,24 @@ public:
     }
     int minimumWhiteTiles(string s, int num, int len) {
         
-        vector<vector<int>> dp(s.size(),vector<int>(num+1,-1));
+        int n = s.size();
+        vector<int> suffix(n);
         
-        return f(0,num,len,s,dp);
+        if(s[n-1]=='1') suffix[n-1]=1;
+        else suffix[n-1]=0;
+        
+        for(int i=n-2; i>=0; i--)
+        {
+            suffix[i] = suffix[i+1];
+            if(s[i]=='1')
+                suffix[i]++;
+        }
+        
+        // for(auto i: suffix)
+        //     cout<<i<<" ";
+        // cout<<endl;
+        vector<vector<int>> dp(n,vector<int>(num+1,-1));
+        
+        return f(0,num,len,s,suffix,dp);
     }
 };
