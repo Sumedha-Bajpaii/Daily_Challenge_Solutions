14cc26c50483e9248d9300c22930db252a3b0ac3
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-03-28 15:35:29 +0530

    Time: 104 ms (5.62%), Space: 6.7 MB (6.48%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    int f(int i, int targ)
+    int f(int i, int targ,vector<vector<int>> &dp)
     {
         if(targ==0 || i==0)
             return 1;
+        if(dp[i][targ] != -1)
+            return dp[i][targ];
         
         int take = 1;
-        if(i<=targ){
-            take = i*f(i,targ-i);
-        }
-        int notTake = f(i-1,targ);
+        if(i<=targ)
+            take = i*f(i,targ-i,dp);
+        
+        int notTake = f(i-1,targ,dp);
+        
         return max(take,notTake);
     }
     
     int integerBreak(int n) 
     {
-        
-        return f(n-1,n);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return f(n-1,n,dp);
     }
 };
