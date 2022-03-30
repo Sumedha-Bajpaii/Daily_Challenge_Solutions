5f1d62c8374dd5bd424b7d2483c028ec2a5e6aed
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-03-30 23:37:54 +0530

    Time: 73 ms (21.49%), Space: 18.5 MB (43.84%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int f(int i, int j, string &s, string &t, vector<vector<int>> &dp){
         
-        if(j==-1)
+        if(j==0)
             return 1;
-        if(i==-1)
+        if(i==0)
             return 0;
         
         if(dp[i][j] != -1)
             return dp[i][j];
         
         int take=0;
-        if(s[i]==t[j])
+        if(s[i-1]==t[j-1])
             take = f(i-1,j-1,s,t,dp);
         
         int notTake = f(i-1,j,s,t,dp);
@@ -23,6 +23,6 @@ public:
         int m=s.length(), n=t.length();
         vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
         
-        return f(m-1,n-1,s,t,dp);
+        return f(m,n,s,t,dp);
     }
 };
