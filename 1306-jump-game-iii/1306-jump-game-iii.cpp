bed4a809df36fa76b4034eb826100093de6ab686
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-04-01 11:58:28 +0530

    Time: 68 ms (41.81%), Space: 31.3 MB (80.96%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    int f(int i,vector<int> &arr){
-        if(i<0 || i>=arr.size())
-            return 0;
-        
-        if(arr[i]==-1)  return 0;
-        if(arr[i]==0)   return 1;
-        
-        int temp = arr[i];
-        arr[i] = -1;
-        int left = f(i-temp,arr);
-        int right = f(i+temp,arr);
-        return left || right;
-    }
     bool canReach(vector<int>& arr, int start) {
         
-        return f(start,arr);
+        int n = arr.size();
+        queue<int> q;
+        q.push(start);
+        
+        while(q.size()){
+            int cur = q.front();
+            q.pop();
+            if(arr[cur]==0)
+                return true;
+            
+            int left = cur-arr[cur];
+            int right = cur+arr[cur];
+            arr[cur] = -1;
+            
+            if(left>=0 && arr[left]!=-1) q.push(left);
+            if(right<n && arr[right]!=-1) q.push(right);
+        }
+        return false;
     }
 };
