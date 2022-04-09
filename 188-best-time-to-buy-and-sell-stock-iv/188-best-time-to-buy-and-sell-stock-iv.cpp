4662a51257bccfeb715b1ed0de6ed866d52e7687
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-04-09 16:35:46 +0530

    Time: 23 ms (38.53%), Space: 13.2 MB (18.64%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -6,21 +6,25 @@ public:
         int n = prices.size();
         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(cnt+1,0)));
         
+        vector<vector<int>> after(2,vector<int>(cnt+1,0));
+        vector<vector<int>> cur(2,vector<int>(cnt+1,0));
+        
         for(int i=n-1; i>=0; i--){
             for(int buy=0; buy<=1; buy++){
                 for(int k=1; k<=cnt; k++){
                     int profit=0;
                     if(buy==1){
-                        profit = max(-prices[i] + dp[i+1][0][k], dp[i+1][buy][k]);
+                        profit = max(-prices[i] + after[0][k], after[buy][k]);
                     }
                     else{
-                        profit = max(prices[i] + dp[i+1][1][k-1], dp[i+1][buy][k]);
+                        profit = max(prices[i] + after[1][k-1], after[buy][k]);
                     }
-                    dp[i][buy][k] = profit;
+                    cur[buy][k] = profit;
                 }
             }
+            after = cur;
         }
         
-        return dp[0][1][cnt];
+        return after[1][cnt];
     }
 };
