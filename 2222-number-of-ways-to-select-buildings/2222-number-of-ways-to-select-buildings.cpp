ae925ca13912ceed020d51526b78846ea414af1b
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-04-15 10:03:54 +0530

    Time: 365 ms (20.97%), Space: 27.9 MB (59.55%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
@@ -1,39 +1,45 @@
 class Solution {
 public:
+//     long long f(int i,int j,string &s,string &x,vector<vector<long long>> &dp){
+        
+//         if(j==0)    return 1;
+//         if(i==0)    return 0;
+        
+//         if(dp[i][j] != -1)
+//             return dp[i][j];
+        
+//         long long t=0, nt=0;
+//         if(s[i-1]==x[j-1])
+//             t = f(i-1,j-1,s,x,dp);
+//         nt = f(i-1,j,s,x,dp);
+        
+//         return dp[i][j] = t+nt;
+//     }
+    
+    long long count(string &s, string x){
+        long long m = s.size();
+        long long n = x.size();
+        
+        long long dp[m+1][n+1];
+        memset(dp,0,sizeof(dp));
+        
+        for(long long i=0; i<=m; i++)   dp[i][0]=1;
+        
+        for(long long i=1; i<=m; i++){
+            for(long long j=1; j<=n; j++){
+                long long t=0, nt=0;
+                if(s[i-1]==x[j-1])
+                    t = dp[i-1][j-1];
+                nt = dp[i-1][j];
+                
+                dp[i][j] = t+nt;
+            }
+        }
+        return dp[m][n];
+    }
+    
     long long numberOfWays(string s) {
         
-        int n = s.size();
-        vector<long long> n01(n,0), n10(n,0);
-        int z=0, one=0;
-        
-        for(int i=0; i<n; i++){
-            if(s[i]=='0')
-                z++;
-            if(i>0){
-                if(s[i]=='1')
-                    n01[i] = n01[i-1] + z;
-                else
-                    n01[i] = n01[i-1];
-            }
-            
-            if(s[i]=='1')
-                one++;
-            if(i>0){
-                if(s[i]=='0')
-                    n10[i] = n10[i-1] + one;
-                else
-                    n10[i] = n10[i-1];
-            }
-        }
-        
-        //x = 010 count     y = 101 count
-        long long x=0, y=0;
-        for(int i=2; i<n; i++){
-            if(s[i]=='0')
-                x += n01[i-1];
-            else
-                y += n10[i-1];
-        }
-        return x+y;
+        return count(s,"010") + count(s,"101");
     }
 };
