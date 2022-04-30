22c3d628469b091c7559f6154064d32a1d8c93ac
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-04-30 18:35:19 +0530

    Added solution - LeetHub

--- Top View of Binary Tree - GFG/top-view-of-binary-tree.cpp
+++ Top View of Binary Tree - GFG/top-view-of-binary-tree.cpp
@@ -108,26 +108,29 @@ class Solution
     {
         //Your code here
         vector<int> res;
+        if(!root)   
+            return res;
+        
         map<int,int> mp;
         queue<pair<Node*,int>> q;
-        
         q.push({root,0});
         
         while(q.size()){
-            auto p = q.front();
-            q.pop();
+            auto p = q.front(); q.pop();
             Node* node = p.first;
             int hd = p.second;
             
-            if(mp.find(hd)==mp.end())
+            if(mp.find(hd) == mp.end())
                 mp[hd] = node->data;
             
             if(node->left)  q.push({node->left,hd-1});
             if(node->right) q.push({node->right,hd+1});
         }
-    
-        for(auto p: mp)
+        
+        for(auto p: mp){
             res.push_back(p.second);
+        }
+        
         return res;
     }
 
