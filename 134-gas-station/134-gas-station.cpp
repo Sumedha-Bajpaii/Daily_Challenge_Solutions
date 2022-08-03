a673ef7a98c7140a209b7a7baf16e506388154ac
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-08-04 00:33:39 +0530

    Time: 113 ms (68.72%), Space: 69.4 MB (52.67%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -3,26 +3,25 @@ public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         
         
-        int tank=0, start=0, i=0, n=gas.size(), round=0, done=1;
+        int tank=0, start=0, i=0, n=gas.size(), round=0, visit=0;
         
-        while(1){
+        while(visit<n){
             if(i==0) round++;
             if(round==3) return -1;
             
+            visit++;
             tank += gas[i];
             tank -= cost[i];
             
             if(tank < 0){   //i can not be start
                 tank = 0;
                 start = (i+1)%n;
-            }
-            else if((i+1)%n==start){
-                return start;
+                visit=0;
             }
             
             i = (i+1)%n;
         }
         
-        return 0;
+        return start;
     }
 };
