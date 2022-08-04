5aec5706ff5c191dfac4f9581952c4927df5e249
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-08-04 12:05:40 +0530

    Time: 5 ms (95.94%), Space: 9.9 MB (92.59%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.cpp
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -2,25 +2,15 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         
-        int n = 0;
+        long int n = 0;
         for(auto num: nums){
             n = n^num;
         }
         
         //n is xor of 2 resultant numbers;
         //find first set bit from right in n;
-        
-        int cnt=0;
-        while((n&1)==0){
-            
-            cnt++;
-            n = n>>1;
-        }
-        
-        int mask=1;
-        mask = mask<<cnt;
-        
-        //cout<<cnt<<" "<<mask<<endl;
+      
+        long long mask = n&-n;
         
         int ans1=0, ans2=0;
         
