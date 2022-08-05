14e9bea26eb08f2972c6012b124bcfd995db6c7d
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-08-05 11:43:13 +0530

    Time: 9 ms (10.81%), Space: 7.7 MB (5.73%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,49 +1,28 @@
 class Solution {
 public:
-    int f(int target,vector<int> &nums,vector<int> &dp){
-        
-        if(target == 0)
-            return 1;
-        if(dp[target] != -1)
-            return dp[target];
-        
-        int ans=0;
-        for(auto x:nums){
-            if(x<=target){
-                ans += f(target-x,nums,dp);
-            }
+    int f(int i,int target,vector<int> &nums,vector<vector<int>> &dp){
+        if(i==nums.size()){
+            if(target==0)
+                return 1;
+            else
+                return 0;
         }
         
-        return dp[target] = ans;
+        if(dp[i][target] != -1)
+            return dp[i][target];
+        
+        int t=0, nt=0;
+        if(nums[i] <= target){
+            t = f(0,target-nums[i],nums,dp);
+        }
+        nt = f(i+1,target,nums,dp);
+        
+        return dp[i][target] = t+nt;
     }
     
     int combinationSum4(vector<int>& nums, int target) {
         
-        vector<int> dp(target+1,-1);
-        return f(target,nums,dp);
+        vector<vector<int>> dp(nums.size()+1,vector<int>(target+1,-1));
+        return f(0,target,nums,dp);
     }
 };
-
-// class Solution {
-// public:
-//     int f(int i,int target,vector<int> &nums){
-//         if(i==nums.size()){
-//             if(target==0)
-//                 return 1;
-//             else
-//                 return 0;
-//         }
-        
-//         int t=0, nt=0;
-//         if(nums[i] <= target){
-//             t = f(i,target-nums[i],nums);
-//         }
-//         nt = f(i+1,target,nums);
-        
-//         return t+nt;
-//     }
-    
-//     int combinationSum4(vector<int>& nums, int target) {
-//         return f(0,target,nums);
-//     }
-// };
