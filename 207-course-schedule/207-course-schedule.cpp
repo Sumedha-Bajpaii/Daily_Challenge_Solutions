eb3c662040f9a0fb8159dfd704bcdedd95f40311
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-08-05 16:44:00 +0530

    Time: 48 ms (23.18%), Space: 14.1 MB (50.47%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -2,46 +2,42 @@ class Solution {
 public:
     bool canFinish(int num, vector<vector<int>>& pre) {
         
-        unordered_map<int,vector<int>> graph;
-        unordered_map<int,int> dep;
-        for(int i=0; i<num; i++){
-            dep[i]=0;
-            graph[i] = vector<int>(0);
-        }
-        for(auto v: pre){
-            graph[v[1]].push_back(v[0]);
-            dep[v[0]]++;
-        }
-        // for(auto p: graph){
-        //     cout<<p.first<<" : ";
-        //     for(auto nbr: p.second)
-        //         cout<<nbr<<" ";
-        //     cout<<endl;
-        // }
+        vector<vector<int>> adj(num);
         
-        // for(auto it: dep){
-        //     cout<<it.first<<" "<<it.second<<endl;
-        // }
-        queue<int> q;
-        for(auto p: dep){
-            if(p.second==0)
-                q.push(p.first);
+        for(auto v:pre){
+            adj[v[1]].push_back(v[0]);
         }
         
-        while(q.size()){
-            int cur = q.front();
-            q.pop();
-            for(auto nbr: graph[cur]){
-                dep[nbr]-=1;
-                if(dep[nbr]==0)
-                    q.push(nbr);
+        vector<int> inDegree(num,0);
+        
+        for(int u=0; u<num; u++){
+            for(auto v: adj[u]){
+                inDegree[v]++;
             }
         }
         
-        for(auto it: dep){
-            if(it.second > 0)
-                return false;
+        queue<int> q;
+        for(int u=0; u<num; u++){
+            if(inDegree[u]==0){
+                q.push(u);
+            }
         }
-        return true;   
+        
+        vector<int> res;
+        while(q.size()){
+            int u = q.front(); q.pop();
+            res.push_back(u);
+            
+            for(auto v: adj[u]){
+                inDegree[v]--;
+                if(inDegree[v]==0){
+                    q.push(v);
+                }
+            }
+        }
+        
+        if(res.size() == num)
+            return true;
+        return false;
     }
 };
