8be526f2489e4060a7322ab393e4bd0f6541a01d
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-08-17 14:29:31 +0530

    Time: 49 ms (19.90%), Space: 15.1 MB (16.19%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
+    bool isCyclic(int node,vector<vector<int>>& adj,vector<int>& vis, vector<int>& dfsvis, vector<int>& res){
+        
+        vis[node] = 1;
+        dfsvis[node] = 1;
+        
+        for(auto it: adj[node]){
+            if(vis[it]==0){
+                if(isCyclic(it,adj,vis,dfsvis,res)==true)
+                    return true;
+            }
+            else if(dfsvis[it]==1)
+                return true;
+        }
+        dfsvis[node] = 0;
+        res.push_back(node);
+        return false;
+    }
+    
     vector<int> findOrder(int num, vector<vector<int>>& pre) {
         
-        vector<int> order;
+        vector<vector<int>> adj(num);
         
-        unordered_map<int,vector<int>> graph;
-        unordered_map<int,int> dep;
+        for(auto v:pre){
+            adj[v[1]].push_back(v[0]);
+        }
+        vector<int> vis(num,0), dfsvis(num,0), res;
         
+        // dfsvis --> true for nodes in current recursion stack
         for(int i=0; i<num; i++){
-            graph[i] = vector<int>(0);
-            dep[i] = 0;
-        }
-        for(auto v: pre){
-            graph[v[1]].push_back(v[0]);
-            dep[v[0]]++;
-        }
-        
-        queue<int> q;
-        for(auto p: dep){
-            if(p.second==0)
-                q.push(p.first);
-        }
-        
-        while(q.size()){
-            int cur = q.front();
-            q.pop();
-            for(auto nbr: graph[cur]){
-                dep[nbr]--;
-                if(dep[nbr]==0)
-                    q.push(nbr);
+            if(vis[i]==0){
+                if(isCyclic(i,adj,vis,dfsvis,res) == true)
+                    return {};
             }
-            order.push_back(cur);
         }
         
-        for(auto p: dep){
-            if(p.second>0)
-                return {};
-        }
-   
-        return order;
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
