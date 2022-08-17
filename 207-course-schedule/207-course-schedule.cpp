49956aff37620d26850e5efac1fcc6d2d4b7ab69
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-08-17 14:24:17 +0530

    Time: 26 ms (79.95%), Space: 14.6 MB (30.81%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    
+    bool isCyclic(int node,vector<vector<int>>& adj,vector<int>& vis, vector<int>& dfsvis){
+        
+        vis[node] = 1;
+        dfsvis[node] = 1;
+        
+        for(auto it: adj[node]){
+            if(vis[it]==0){
+                if(isCyclic(it,adj,vis,dfsvis)==true)
+                    return true;
+            }
+            else if(dfsvis[it]==1)
+                return true;
+        }
+        dfsvis[node] = 0;
+        return false;
+    }
+    
     bool canFinish(int num, vector<vector<int>>& pre) {
         
         vector<vector<int>> adj(num);
@@ -7,37 +25,17 @@ public:
         for(auto v:pre){
             adj[v[1]].push_back(v[0]);
         }
+        vector<int> vis(num,0), dfsvis(num,0), res;
         
-        vector<int> inDegree(num,0);
-        
-        for(int u=0; u<num; u++){
-            for(auto v: adj[u]){
-                inDegree[v]++;
+        // dfsvis --> true for nodes in current recursion stack
+        for(int i=0; i<num; i++){
+            if(vis[i]==0){
+                if(isCyclic(i,adj,vis,dfsvis) == true)
+                    return false;
             }
         }
-        
-        queue<int> q;
-        for(int u=0; u<num; u++){
-            if(inDegree[u]==0){
-                q.push(u);
-            }
-        }
-        
-        vector<int> res;
-        while(q.size()){
-            int u = q.front(); q.pop();
-            res.push_back(u);
-            
-            for(auto v: adj[u]){
-                inDegree[v]--;
-                if(inDegree[v]==0){
-                    q.push(v);
-                }
-            }
-        }
-        
-        if(res.size() == num)
-            return true;
-        return false;
+     
+        return true;
+
     }
 };
