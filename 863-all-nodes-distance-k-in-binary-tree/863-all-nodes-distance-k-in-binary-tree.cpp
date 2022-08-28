c027149fa51b1dcd865e964577031d768fcb507c
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-08-28 17:05:48 +0530

    Time: 14 ms (32.14%), Space: 13 MB (61.90%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -29,35 +29,48 @@ public:
         }
     }
     
-    void dfs(TreeNode* node,unordered_map<TreeNode*,TreeNode*>& parent, int dist, int k,unordered_set<TreeNode*>& vis, vector<int>& res){
-        
-        if(dist==k){
-            res.push_back(node->val);
-            return;
-        }
-        vis.insert(node);
-        //parent left right
-        
-        if(parent.find(node)!=parent.end() && vis.find(parent[node])==vis.end()){
-            dfs(parent[node],parent,dist+1,k,vis,res);
-        }
-        if(node->left && vis.find(node->left)==vis.end()){
-            dfs(node->left, parent, dist+1,k,vis,res);
-        }
-        if(node->right && vis.find(node->right)==vis.end()){
-            dfs(node->right, parent, dist+1,k,vis,res);
-        }
-    }
-    
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         
         unordered_map<TreeNode*,TreeNode*> parent;
         record_parent(root, parent);
         
+        //bfs
         unordered_set<TreeNode*> vis;
-        vector<int> res;
-        dfs(target,parent,0,k,vis,res);
+        queue<TreeNode*> q;
+        q.push(target);
+        vis.insert(target);
+        int dist=0;
         
+        while(q.size()){
+            if(dist==k)
+                break;
+            dist++;
+            
+            int sz = q.size();
+            for(int i=0; i<sz; i++){
+                auto node = q.front();
+                q.pop();
+
+                if(parent.find(node)!=parent.end() && vis.find(parent[node])==vis.end()){
+                    q.push(parent[node]);
+                    vis.insert(parent[node]);
+                }
+                if(node->left && vis.find(node->left)==vis.end()){
+                    q.push(node->left);
+                    vis.insert(node->left);
+                }
+                if(node->right && vis.find(node->right)==vis.end()){
+                    q.push(node->right);
+                    vis.insert(node->right);
+                }
+            }
+        }
+        
+        vector<int> res;
+        while(q.size()){
+            res.push_back(q.front()->val);
+            q.pop();
+        }
         return res;
     }
 };
