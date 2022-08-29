2049c915beb25cfe0df3ffbe87701e061b7d98cd
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-08-29 14:15:46 +0530

    Time: 66 ms (94.18%), Space: 19.1 MB (96.00%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -6,35 +6,42 @@ public:
         if(grid[0][0] != 0)
             return -1;
         
-        vector<vector<int>> dist(m,vector<int>(n,INT_MAX));
-        dist[0][0] = 1;
+        //vector<vector<int>> dist(m,vector<int>(n,INT_MAX));
+        // dist[0][0] = 1;
         
+        int steps = 0;
         queue<pair<int,int>> q;
         q.push({0,0});
         
         while(!q.empty()){
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
+        
+            steps++;
+            int sz = q.size();
             
-            for(int i=-1; i<=1; i++){
-                for(int j=-1; j<=1; j++){
-                    // if(i==0 && j==0)
-                    //     continue;
-                    if(x+i<0 or x+i>=m or y+j<0 or y+j>=n)
-                        continue;
-                    if(grid[x+i][y+j]==1)
-                        continue;
-                    if(dist[x+i][y+j] > dist[x][y]+1){
-                        dist[x+i][y+j] = dist[x][y]+1;
+            for(int k=0; k<sz; k++){
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+                
+                if(x==m-1 && y==n-1)
+                    return steps;
+                
+                for(int i=-1; i<=1; i++){
+                    for(int j=-1; j<=1; j++){
+                        // if(i==0 && j==0)
+                        //     continue;
+                        if(x+i<0 or x+i>=m or y+j<0 or y+j>=n)
+                            continue;
+                        if(grid[x+i][y+j]==1)
+                            continue;
+                        
                         q.push({x+i,y+j});
+                        grid[x+i][y+j] = 1;
                     }
                 }
             }
         }
         
-        if(dist[m-1][n-1] == INT_MAX)
-            return -1;
-        return dist[m-1][n-1];
+        return -1;
     }
 };
