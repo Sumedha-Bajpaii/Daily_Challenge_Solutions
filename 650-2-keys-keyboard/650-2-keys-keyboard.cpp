d54ca1fb7cb9dd8cee200dcc9e94756d15906cb7
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-09-07 21:00:21 +0530

    Time: 22 ms (52.22%), Space: 5.9 MB (88.84%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,9 +1,36 @@
+// at each step we have 2 options
+// paste   --> operation += 1   --> len += prev
+// copy paste  --> operation += 2   --> len += len
+
+class Solution {
+public:
+    int f(int len, int prev, int n){
+        if(len > n)
+            return 1e9;
+        if(len == n)
+            return 0;
+        
+        int p = 1 + f(len+prev, prev, n);
+        int cp = 2 + f(len+len, len, n);
+        return min(p,cp);
+    }
+    
+    int minSteps(int n) {
+        if(n==1)    return 0;
+        return 1 + f(1,1,n);
+    }
+};
+
+
+
+
+
 //copy copy F
 //copy paste T
 //paste paste T
 //paste copy T
 
-class Solution {
+class Solution1 {
 public:
     int f(int curLen, int lastCopied, char lastOp, int n){
         
