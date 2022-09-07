19a743ebca905d2ccd2d35c6eeb26f84e3bf54ea
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-09-07 16:03:06 +0530

    Time: 15 ms (47.99%), Space: 12.9 MB (8.03%) - LeetHub

---
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int mod = 1e9+7;
+    long dfs(int i,int j, int maxx,int m,int n,vector<vector<vector<int>>> &dp){
+        
+        if(i<0 || j<0 || i>=m || j>=n)
+            return 1;
+        if(maxx==0)
+            return 0;
+        if(dp[i][j][maxx] != -1)
+            return dp[i][j][maxx];
+        
+        long l=0,r=0,u=0,d=0;
+        l = dfs(i,j-1, maxx-1,m,n,dp);
+        r = dfs(i,j+1, maxx-1,m,n,dp);
+        u = dfs(i-1,j, maxx-1,m,n,dp);
+        d = dfs(i+1,j, maxx-1,m,n,dp);
+        
+        return dp[i][j][maxx] = (l+r+u+d)%mod;
+        
+    }
+    
+    int findPaths(int m, int n, int maxx, int sr, int sc) {
+        
+        vector<vector<vector<int>>> dp(m+1,vector<vector<int>>(n+1,vector<int>(51,-1)));
+        return dfs(sr,sc,maxx,m,n,dp);
+    }
+};
