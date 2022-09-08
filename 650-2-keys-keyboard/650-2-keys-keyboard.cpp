558714353275c1436f0a4bc87f98ec24a0e90d8d
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-09-08 11:26:03 +0530

    Time: 251 ms (5.48%), Space: 498.4 MB (8.57%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -4,59 +4,23 @@
 
 class Solution {
 public:
-    int f(int len, int prev, int n){
+    int f(int len, int prev, int n,vector<vector<int>> &dp){
         if(len > n)
             return 1e9;
         if(len == n)
             return 0;
         
-        int p = 1 + f(len+prev, prev, n);
-        int cp = 2 + f(len+len, len, n);
-        return min(p,cp);
+        if(dp[len][prev] != -1)
+            return dp[len][prev];
+        
+        int p = 1 + f(len+prev, prev, n,dp);
+        int cp = 2 + f(len+len, len, n,dp);
+        return dp[len][prev] = min(p,cp);
     }
     
     int minSteps(int n) {
         if(n==1)    return 0;
-        return 1 + f(1,1,n);
-    }
-};
-
-
-
-
-
-//copy copy F
-//copy paste T
-//paste paste T
-//paste copy T
-
-class Solution1 {
-public:
-    int f(int curLen, int lastCopied, char lastOp, int n){
-        
-        if(curLen == n)
-            return 0;
-        if(curLen > n)
-            return 1e9;
-        
-        int x=1e9,y=1e9,z=1e9;
-        
-        if(lastOp == 'c'){
-            x = 1 + f(curLen+lastCopied, lastCopied, 'p', n);   //copy -> paste
-        }
-        if(lastOp == 'p'){
-            y = 1 + f(curLen+lastCopied, lastCopied, 'p', n);   //paste -> paste
-            z = 1+ f(curLen, curLen, 'c', n);                  //paste ->copy
-        }
-        
-        return min({x,y,z});
-    }
-    
-    int minSteps(int n) {
-        
-        if(n==1)
-            return 0;
-        int res = 1 + f(1,1,'c',n);
-        return res;   //+1 for initial copy operation
+        vector<vector<int>> dp(2*n,vector<int>(2*n,-1));
+        return 1 + f(1,1,n,dp);
     }
 };
