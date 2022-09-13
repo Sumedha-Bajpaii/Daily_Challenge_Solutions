13cf11104ae3602bd151167e12ff31b0bc90ff22
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-09-13 23:37:36 +0530

    Time: 43 ms (47.37%), Space: 13.6 MB (61.15%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    bool dfs(int u,int parent,vector<int>& vis,vector<vector<int>>& graph){
+    bool bfs(int u,vector<int>& vis,vector<vector<int>>& graph){
         
-        if(parent==2)
-            vis[u] = -1;    //first color
-        else
-            vis[u] = parent*-1;
+        queue<int> q;
+        q.push(u);
+        vis[u] = 0;
         
-        for(auto it: graph[u]){
-            if(vis[it]==0){
-                if(dfs(it,vis[u],vis,graph)==false)
+        while(q.size()){
+            int v = q.front();
+            q.pop();
+            
+            for(auto it: graph[v]){
+                if(vis[it] == -1){
+                    q.push(it);
+                    vis[it] = !vis[v];
+                }
+                else if(vis[it] == vis[v])
                     return false;
             }
-            else if(vis[it]==vis[u])
-                return false;
         }
-        
         return true;
     }
+    
     bool isBipartite(vector<vector<int>>& graph) {
         
         int n = graph.size();
-        vector<int> vis(n,0); //0: not visited    1/-1: two colors
+        vector<int> vis(n,-1); //-1: not visited    1/0: two colors
         
         for(int i=0; i<n; i++){
-            if(vis[i]==0){     //not visited
-                if(dfs(i,2,vis,graph)==false)
+            if(vis[i]==-1){     //not visited
+                if(bfs(i,vis,graph)==false)
                     return false;
             }
         }
