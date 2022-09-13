608c43daf5e1c882f7a61e3a88718836228a9f3b
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-09-13 14:23:23 +0530

    Time: 41 ms (57.02%), Space: 13.3 MB (60.13%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -10,30 +10,30 @@
  */
 class Solution {
 public:
+    ListNode* merge2lists(ListNode* l1, ListNode* l2){
+        
+        if(l1==NULL)    return l2;
+        if(l2==NULL)    return l1;
+        
+        if(l1->val <= l2->val){
+            l1->next = merge2lists(l1->next,l2);
+            return l1;
+        }
+        l2->next = merge2lists(l1,l2->next);
+        return l2;
+    }
+    
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
-//lists: vector of head pointers of linkedLists.... not a vector of whole list
-        priority_queue<pair<int,ListNode*>, vector<pair<int,ListNode*>>, greater<pair<int,ListNode*>>> minHeap;
-        
-        for(ListNode *head: lists){
-            if(head != NULL)
-                minHeap.push({head->val,head});
+        if(lists.size()==0)
+            return NULL;
+        int len = lists.size();
+        while(len > 1){   
+            for(int i=0; i<len/2; i++){
+                lists[i] = merge2lists(lists[i], lists[len-1-i]);
+            }
+            len = (len+1)/2;
         }
-        
-        ListNode *start = new ListNode(), *cur = start;
-        
-        while(!minHeap.empty()){
-            ListNode* ptr = minHeap.top().second;
-            minHeap.pop();
-            
-            cur->next = ptr;
-            cur = cur->next;
-            ptr = ptr->next;
-            
-            if(ptr != NULL)
-                minHeap.push({ptr->val,ptr});
-        }
-        
-        return start->next;
+        return lists[0];
     }
 };
