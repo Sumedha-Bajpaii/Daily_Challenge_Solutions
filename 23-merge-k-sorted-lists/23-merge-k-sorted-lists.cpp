147929bbf04bd15b537deec8adaca36a1101e096
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-09-13 11:44:58 +0530

    Time: 56 ms (37.57%), Space: 13.6 MB (39.92%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -8,37 +8,32 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class compare{
-    public:
-        bool operator()(ListNode* a,ListNode* b){
-            return a->val > b->val;
-        }
-};
-
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
-        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+//lists: vector of head pointers of linkedLists.... not a vector of whole list
+        priority_queue<pair<int,ListNode*>, vector<pair<int,ListNode*>>, greater<pair<int,ListNode*>>> minHeap;
         
-        for(auto &head: lists)
-            if(head)
-                pq.push(head);
-        
-        ListNode *dummy = new ListNode(0);
-        ListNode *tail = dummy;
-        
-        while(!pq.empty()){
-            ListNode *cur = pq.top();
-            pq.pop();
-            
-            if(cur->next!=nullptr)
-                pq.push(cur->next);
-            
-            tail->next = cur;
-            tail = tail->next; 
+        for(ListNode *head: lists){
+            if(head != NULL)
+                minHeap.push({head->val,head});
         }
         
-        return dummy->next;
+        ListNode *start = new ListNode(), *cur = start;
+        
+        while(!minHeap.empty()){
+            ListNode* ptr = minHeap.top().second;
+            minHeap.pop();
+            
+            cur->next = ptr;
+            cur = cur->next;
+            ptr = ptr->next;
+            
+            if(ptr != NULL)
+                minHeap.push({ptr->val,ptr});
+        }
+        
+        return start->next;
     }
 };
