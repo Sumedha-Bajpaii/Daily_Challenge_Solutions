12d7d8722c446b774b8c65ac140db2e78a91557e
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-09-14 19:22:44 +0530

    Time: 753 ms (28.17%), Space: 181.1 MB (57.54%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void f(TreeNode* root, vector<int>& freq, int& odd,int &path){
+        
+        if(root==NULL)  return;
+        if(!root->left && !root->right){    //leaf
+            if(++freq[root->val]%2 != 0)  odd++;
+            else  odd--;
+            
+            if(odd<=1) path++;
+            
+            if(--freq[root->val]%2 != 0)  odd++;
+            else  odd--;
+        }
+        
+        if(++freq[root->val]%2 != 0)  odd++;
+        else  odd--;
+        
+        f(root->left,freq,odd,path);
+        f(root->right,freq,odd,path);
+        
+        if(--freq[root->val]%2 != 0)  odd++;
+        else  odd--;
+ 
+    }
+    int pseudoPalindromicPaths (TreeNode* root) {
+        
+        vector<int> freq(10,0);
+        int odd=0, path=0;
+        f(root,freq,odd,path);
+        
+        return path;
+    }
+};
