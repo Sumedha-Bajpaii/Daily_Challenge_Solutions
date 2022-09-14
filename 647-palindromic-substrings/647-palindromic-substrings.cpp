f6353dda2f7e2dfcaf3435cad640aca6931ab376
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-09-14 20:39:02 +0530

    Time: 13 ms (62.78%), Space: 7.4 MB (60.29%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
-    int check(int left,int right,string s){
-        int count=0;
-        while(left>=0 && right<s.size() && s[left]==s[right]){
-            count++; left--; right++;
-        }
-        return count;
-    }
     int countSubstrings(string s) {
         
-        int ans=0;
-        for(int i=0; i<s.size(); i++){
-            ans += check(i,i,s);       //odd palindrome
-            ans += check(i,i+1,s);     //even pal
+        int n = s.size(), count=0;
+        bool dp[n][n];
+        
+        for(int gap=0; gap<n; gap++){
+            int i,j;
+            for(i=0, j=gap; j<n ; i++,j++){
+                if(gap==0)
+                    dp[i][j] = true;
+                else if(gap==1){
+                    if(s[i] == s[j])
+                        dp[i][j] = true;
+                    else
+                        dp[i][j] = false;
+                }
+                else{
+                    if(s[i]==s[j] && dp[i+1][j-1]==true){
+                        dp[i][j] = true;
+                    }
+                    else{
+                        dp[i][j] = false;
+                    }
+                }
+                
+                if(dp[i][j]==true)
+                    count++;
+            }
         }
-        return ans;
+        
+        return count;
     }
 };
