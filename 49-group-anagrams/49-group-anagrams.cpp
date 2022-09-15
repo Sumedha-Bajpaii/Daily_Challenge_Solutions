eaf8848d2989aad2df7ef36f329b465749a22a67
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-09-15 20:20:49 +0530

    Time: 119 ms (14.17%), Space: 20.7 MB (56.85%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -2,20 +2,35 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-        map<string, vector<string>> mp;
+        unordered_map<string, vector<string>> mp;
         
         for(auto s: strs){
-            string temp = s;
-            sort(temp.begin(),temp.end());
+            // string temp = s;
+            // sort(temp.begin(),temp.end());
+            //sort s using counting sort as s has only 26 different characters
+            string temp = mySort(s);
             mp[temp].push_back(s);
         }
         
         vector<vector<string>> ans;
-        
         for(auto p: mp){
             ans.push_back(p.second);
         }
         
         return ans;
+    }
+    
+    string mySort(string s){
+        
+        int freq[26] = {0};
+        for(int i=0; i<s.size(); i++)
+            freq[s[i]-'a']++;
+        
+        string t = "";
+        for(int i=0; i<26; i++){
+            t += string(freq[i],i+'a');
+        }
+        
+        return t;
     }
 };
