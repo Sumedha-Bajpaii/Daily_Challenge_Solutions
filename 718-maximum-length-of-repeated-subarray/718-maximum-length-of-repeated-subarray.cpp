293d49212c6d4388e81d71f2d23c2178af141903
Sumedha Bajpai <72724079+Sumedha-Bajpaii@users.noreply.github.com>
2022-09-17 01:11:46 +0530

    Time: 359 ms (74.45%), Space: 16 MB (88.82%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,28 +1,23 @@
+//longest common substring
 class Solution {
 public:
-    int ans=0;
-    int f(int i,int j,vector<int>& A,vector<int>& B,vector<vector<int>>& dp){
-        
-        if(i<0 || j<0)  return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int cur=0;
-        if(A[i]==B[j]){
-            cur = 1 + f(i-1,j-1,A,B,dp);
-        }
-        ans = max(ans,cur);
-        f(i-1,j,A,B,dp);
-        f(i,j-1,A,B,dp);
-        
-        return dp[i][j] = cur;
-    }
-    
     int findLength(vector<int>& A, vector<int>& B) {
         
-        int n=A.size(), m=B.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        f(n-1,m-1,A,B,dp);
-        return ans;
+        int n=A.size(), m=B.size(), res=0;
+        
+        int dp[n+1][m+1];
+        memset(dp,0,sizeof(dp));
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(A[i-1]==B[j-1])
+                    dp[i][j] = 1+dp[i-1][j-1];
+                else
+                    dp[i][j] = 0;
+                res = max(res,dp[i][j]);
+            }
+        }
+        
+        return res;
     }
 };
